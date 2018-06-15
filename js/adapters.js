9f89b21d908b27d03c06a269dc8d8759b0562152
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-06-15 14:24:28 -0400

    finish launch adapter

--- js/adapters.js
+++ js/adapters.js
@@ -5,7 +5,7 @@ const RocketAdapter = {
     return fetch(this.url).then(r => r.json())
       .then(rockets=> {
         return rockets.map((rocket, index) => {
-          let rocketInfo = {id: index + 1},
+          let rocketInfo = {rocket_id: rocket.id, id: index + 1 },
             picks = ["name", "description", "success_rate_pct", "height", "mass","stages"];
           for (attr in rocket) {
             if (picks.includes(attr)){
@@ -24,5 +24,25 @@ const RocketAdapter = {
 // 4 ={id: "bfr", name: "Big Falcon Rocket", type: "rocket", active: false, stages: 2, …}
     return this.getAll()
       .then(rockets => rockets[num - 1])
+  }
+}
+
+const LaunchAdapter = {
+  url: "https://api.spacexdata.com/v2/launches/all",
+  getAll: function() {
+    return fetch(this.url)
+      .then(r => r.json())
+      .then(launches=> {
+        return launches.map((launch, index) => {
+          let launchInfo = {rocket_id: launch.rocket.rocket_id, id: launch.flight_number }
+            picks = ["mission_name", "launch_success", "launch_date_utc"];
+          for (attr in launch) {
+            if (picks.includes(attr)){
+              launchInfo[attr] = launch[attr]
+            }
+          }
+          return launchInfo
+        })
+      })
   }
 }
